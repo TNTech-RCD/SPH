d21f4cd1843e560d134968fd38860bfe6adee953
Tyler Allen <t.allen@charlotte.edu>
2024-05-24 21:08:20 -0400

    Fixing run script to ensure blink lights are enabled. Re-enabling lighting in the git version

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -xe
 
 #mpirun -x LD_LIBRARY_PATH --hostfile ./hostfile -np 4 -N 1 --mca btl_tcp_if_include eth0 /home/demo/bin/sph.out
-mpirun -np 4 -N 1 --hostfile ./hostfile ./bin/sph.out
+
+hostfile=./hostfile
+for p in `cat ${hostfile}`; do
+  ssh "`whoami`@$p" "sudo `pwd`/blink1-tool/blink1-tool --add_udev_rules"
+  echo "$p"
+done
+mpirun -np 4 -N 1 --hostfile ${hostfile} ./bin/sph.out
